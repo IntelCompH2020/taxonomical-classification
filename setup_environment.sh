c9ed99306b2b110da92a385483f68cdbf1bd9e44
joanllop <jlloppalao@gmail.com>
2022-09-27 12:46:31 +0200

    Added amd setup

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,5 +1,10 @@
-python3 -m venv venv
+#!/bin/bash
 
-source venv/bin/activate
-
-pip install -r utils/requirements.txt
+if uname -a | grep -q amd
+then
+	bash /gpfs/scratch/bsc88/bsc88148/amd_setup/setup.sh -n venv
+else
+	python3 -m venv venv
+	source venv/bin/activate
+	pip install -r utils/requirements.txt
+fi
